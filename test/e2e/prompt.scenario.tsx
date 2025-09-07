7e2efedd7330e27d2f57a190fa023709ea1cc749
neeppy <cosmin.stoica97@gmail.com>
2025-09-07 21:12:03 +0300

    feat: move to vitest, add linting & formatting (#6)

--- test/e2e/prompt.scenario.tsx
+++ test/e2e/prompt.scenario.tsx
@@ -2,43 +2,55 @@ import React, { PropsWithChildren, useState } from 'react';
 import createModalManager from '../../lib/createModalManager';
 
 const DefaultVariant = ({ children }: PropsWithChildren) => (
-    <div data-testid="defaultVariant">
-        {children}
-    </div>
+  <div data-testid="defaultVariant">{children}</div>
 );
 
 const { store, prompt } = createModalManager({
-    variants: { default: DefaultVariant },
+  variants: { default: DefaultVariant },
 });
 
 const PaymentModal = ({ close }: any) => {
-    const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
-    return (
-        <div>
-            <input type="number" data-testid="amountField" value={amount} onChange={e => setAmount(e.target.valueAsNumber)} />
-            <button data-testid="closeBtn" onClick={() => close()}>Close</button>
-            <button data-testid="submitBtn" onClick={() => close(true, amount)}>
-                Submit
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="number"
+        data-testid="amountField"
+        value={amount}
+        onChange={(e) => setAmount(e.target.valueAsNumber)}
+      />
+      <button data-testid="closeBtn" onClick={() => close()}>
+        Close
+      </button>
+      <button data-testid="submitBtn" onClick={() => close(true, amount)}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
-const ReactApp = ({ onSuccess, onCancel }: { onSuccess: (amount: number) => void, onCancel: () => void }) => (
-    <div>
-        <button
-            data-testid="payButton"
-            onClick={async () => {
-                const { isConfirm, data: paidAmount } = await prompt<number>(PaymentModal);
+const ReactApp = ({
+  onSuccess,
+  onCancel,
+}: {
+  onSuccess: (amount: number) => void;
+  onCancel: () => void;
+}) => (
+  <div>
+    <button
+      data-testid="payButton"
+      onClick={async () => {
+        const { isConfirm, data: paidAmount } =
+          await prompt<number>(PaymentModal);
 
-                if (isConfirm) onSuccess(paidAmount);
-                else onCancel();
-            }}
-        >
-            Pay
-        </button>
-    </div>
+        if (isConfirm) onSuccess(paidAmount);
+        else onCancel();
+      }}
+    >
+      Pay
+    </button>
+  </div>
 );
 
 export { store, ReactApp };
--- test/store.test.ts
+++ test/store.test.ts
@@ -1,29 +1,32 @@
 import { createStore } from '../lib/store';
+import { describe, it, expect, vi } from 'vitest';
 
 const DefaultVariant = () => null;
 const ContentComponent = () => null;
 
 describe('store', () => {
-    it('calls subscriber when a change is done', () => {
-        const store = createStore();
-        const listener = jest.fn();
+  it('calls subscriber when a change is done', () => {
+    const store = createStore();
+    const listener = vi.fn();
 
-        store.subscribe(listener);
-        const key = store.open(ContentComponent, {
-            variantComponent: DefaultVariant,
-        });
-
-        expect(listener).toHaveBeenCalledWith([{
-            key: expect.any(String),
-            variantComponent: DefaultVariant,
-            contentComponent: ContentComponent,
-            props: {},
-            settings: {},
-        }]);
-
-        store.close(key);
-
-        expect(listener).toHaveBeenCalledWith([]);
-        expect(listener).toHaveBeenCalledTimes(2);
+    store.subscribe(listener);
+    const key = store.open(ContentComponent, {
+      variantComponent: DefaultVariant,
     });
+
+    expect(listener).toHaveBeenCalledWith([
+      {
+        key: expect.any(String),
+        variantComponent: DefaultVariant,
+        contentComponent: ContentComponent,
+        props: {},
+        settings: {},
+      },
+    ]);
+
+    store.close(key);
+
+    expect(listener).toHaveBeenCalledWith([]);
+    expect(listener).toHaveBeenCalledTimes(2);
+  });
 });
